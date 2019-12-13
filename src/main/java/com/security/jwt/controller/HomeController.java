eba81b49154dcca38083ff287f52c665c076f154
dsmirnov <dsmirnov@s-pro.io>
2019-12-13 10:07:01 +0200

    init

---
+++ src/main/java/com/security/jwt/controller/HomeController.java
@@ -0,0 +1,20 @@
+package com.security.jwt.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.RequestEntity;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
+
+@RestController
+@RequestMapping("/home")
+public class HomeController {
+
+    @GetMapping
+    public ResponseEntity hello(HttpServletRequest request) {
+        return new ResponseEntity("Hello" + request.toString(), HttpStatus.OK);
+    }
+}
